ec7dd5076ec218d0a2cb6ec70a09c8669d4e6b27
thinca <thinca@gmail.com>
2011-12-01 11:04:35 +0900

    Improved for multibyte character.
    In previous version, text was treated as ascii.

--- coffee.bat
+++ coffee.bat
@@ -75,28 +75,64 @@ function loadCoffee() {
   return this.CoffeeScript;
 }
 
-function readFile(file) {
-  if (!FSO.FileExists(file)) {
-    throw new Error("File not found: " + file);
+var readFile = (function() {
+  // some characters are broken in 'iso-8859-1'.
+  var illegalChars = {
+    0x20ac: 0x80, 0x81  : 0x81, 0x201a: 0x82, 0x192 : 0x83, 0x201e: 0x84,
+    0x2026: 0x85, 0x2020: 0x86, 0x2021: 0x87, 0x2c6 : 0x88, 0x2030: 0x89,
+    0x160 : 0x8a, 0x2039: 0x8b, 0x152 : 0x8c, 0x8d  : 0x8d, 0x17d : 0x8e,
+    0x8f  : 0x8f, 0x90  : 0x90, 0x2018: 0x91, 0x2019: 0x92, 0x201c: 0x93,
+    0x201d: 0x94, 0x2022: 0x95, 0x2013: 0x96, 0x2014: 0x97, 0x2dc : 0x98,
+    0x2122: 0x99, 0x161 : 0x9a, 0x203a: 0x9b, 0x153 : 0x9c, 0x9d  : 0x9d,
+    0x17e : 0x9e, 0x178 : 0x9f
+  };
+  return function(file) {
+    if (!FSO.FileExists(file)) {
+      throw new Error("File not found: " + file);
+    }
+    if (FSO.getFile(file).Size == 0) {
+      return "";
+    }
+    // read in binary
+    var stream = WScript.CreateObject("ADODB.Stream");
+    try {
+      stream.Type = 2;  // adTypeText
+      stream.Charset = 'iso-8859-1';
+      stream.Open();
+      stream.LoadFromFile(file);
+      var text = stream.readText();
+      var list = [];
+      for (var i = 0; i < text.length; i++) {
+        var v = text.charCodeAt(i);
+        list.push(illegalChars[v] || v);
+      }
+      return String.fromCharCode.apply(null, list);
+    } finally {
+      stream.Close();
+    }
   }
-  if (FSO.getFile(file).Size == 0) {
-    return "";
-  }
-  var stream = FSO.OpenTextFile(file, 1);
+})();
+
+function writeFile(file, content) {
+  var stream = WScript.CreateObject('ADODB.Stream');
   try {
-    return stream.ReadAll();
+    stream.Charset = 'iso-8859-1';
+    stream.Open();
+    stream.WriteText(content);
+    stream.SaveToFile(file, 2);
   } finally {
     stream.Close();
   }
 }
 
-function writeFile(file, content) {
-  var stream = FSO.OpenTextFile(file, 2, true);
-  try {
-    return stream.Write(content);
-  } finally {
-    stream.Close();
-  }
+function binaryToString(binary, charset) {
+  var stream = WScript.CreateObject('ADODB.Stream');
+  stream.Charset = 'iso-8859-1';
+  stream.Open();
+  stream.WriteText(binary);
+  stream.Position = 0;
+  stream.Charset = charset || '_autodetect_all';
+  return stream.ReadText();
 }
 
 function parseArguments() {
@@ -279,7 +315,7 @@ function main() {
     } else if (o.compile) {
       var compiled = CoffeeScript.compile(src, compileOptions);
       if (o.print) {
-        WScript.Echo(compiled);
+        WScript.StdOut.Write(binaryToString(compiled));
       } else if (file) {
         var js = file.replace(/(\.\w+)?$/, ".js");
         if (o.output) {
